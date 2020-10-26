1e13968c81507c0d1fcf64b804000faa0661acc6
Gianluca Coletti <55032531+GColetti@users.noreply.github.com>
2020-10-26 16:29:52 -0400

    Changed Viewcontroller and LaunchScreen

--- AssistiveButtons/ViewController.swift
+++ AssistiveButtons/ViewController.swift
@@ -6,19 +6,86 @@
 //
 
 import UIKit
-import RealityKit
+import SceneKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
     
-    @IBOutlet var arView: ARView!
+    @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        // Set the view's delegate
+        sceneView.delegate = self
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARImageTrackingConfiguration()
+        
+        // first see if there is a folder called "ARImages" Resource Group in our Assets Folder
+        if let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "ARImages", bundle: Bundle.main) {
+            
+            // if there is, set the images to track
+            configuration.trackingImages = trackedImages
+            // at any point in time, only 1 image will be tracked
+            configuration.maximumNumberOfTrackedImages = 1
+        }
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        // if the anchor is not of type ARImageAnchor (which means image is not detected), just return
+        guard let imageAnchor = anchor as? ARImageAnchor, let fileUrlString = Bundle.main.path(forResource: "buttonLocated", ofType: "png") else {return}
+        
+        //find our video file
+        let videoItem = AVPlayerItem(url: URL(fileURLWithPath: fileUrlString))
+        
+        let player = AVPlayer(playerItem: videoItem)
+        //initialize video node with avplayer
+        let videoNode = SKVideoNode(avPlayer: player)
+        
+        player.play()
+        // add observer when our player.currentItem finishes player, then start playing from the beginning
+        NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: player.currentItem, queue: nil) { (notification) in
+            player.seek(to: CMTime.zero)
+            player.play()
+            print("Looping Video")
+        }
+        
+        // set the size (just a rough one will do)
+        let videoScene = SKScene(size: CGSize(width: 480, height: 360))
+        // center our video to the size of our video scene
+        videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
+        // invert our video so it does not look upside down
+        videoNode.yScale = -1.0
+        // add the video to our scene
+        videoScene.addChild(videoNode)
+        // create a plan that has the same real world height and width as our detected image
+        let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+        // set the first materials content to be our video scene
+        plane.firstMaterial?.diffuse.contents = videoScene
+        // create a node out of the plane
+        let planeNode = SCNNode(geometry: plane)
+        // since the created node will be vertical, rotate it along the x axis to have it be horizontal or parallel to our detected image
+        planeNode.eulerAngles.x = -Float.pi / 2
+        // finally add the plane node (which contains the video node) to the added node
+        node.addChildNode(planeNode)
     }
 }
