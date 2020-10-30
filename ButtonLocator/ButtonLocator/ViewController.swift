b8c1bffffeaaf77f887f7e87a633ed3b1a984882
thuyvi-le <thuyvi.le.canada@gmail.com>
2020-10-30 10:13:07 -0400

    Added object recognition code

--- ButtonLocator/ButtonLocator/ViewController.swift
+++ ButtonLocator/ButtonLocator/ViewController.swift
@@ -11,7 +11,118 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        import UIKit
+        import Vision
+
+        //Configure camera to use for capture
+        private let session = AVCaptureSession()
+
+        //Set up device and session resoltion
+        let videoDevice = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .back).devices.first
+        do {
+            deviceInput = try AVCaptureDeviceInput(device: videoDevice!)
+        } catch {
+            print("Could not create video device input: \(error)")
+            return
+        }
+
+        session.beginConfiguration()
+        session.sessionPreset = .vga640x480 //See if resolution can be made smaller
+
+        //Add video input to session by adding the camera as a device
+        guard session.canAddInput(deviceInput) else {
+            print("Could not add video device input to the session")
+            session.commitConfiguration()
+            return
+        }
+        session.addInput(deviceInput)
+
+        //Add video output to your session
+        if session.canAddOutput(videoDataOutput) {
+            session.addOutput(videoDataOutput)
+            // Add a video data output
+            videoDataOutput.alwaysDiscardsLateVideoFrames = true
+            videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+            videoDataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
+        } else {
+            print("Could not add video data output to the session")
+            session.commitConfiguration()
+            return
+        }
+
+        //Processing frame
+        let captureConnection = videoDataOutput.connection(with: .video)
+        // Always process the frames
+        captureConnection?.isEnabled = true
+        do {
+            try  videoDevice!.lockForConfiguration()
+            let dimensions = CMVideoFormatDescriptionGetDimensions((videoDevice?.activeFormat.formatDescription)!)
+            bufferSize.width = CGFloat(dimensions.width)
+            bufferSize.height = CGFloat(dimensions.height)
+            videoDevice!.unlockForConfiguration()
+        } catch {
+            print(error)
+        }
+
+        //Commit session configuration
+        session.commitConfiguration()
+
+        //Set up preview layer on view controller
+        previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        rootLayer = previewView.layer
+        previewLayer.frame = rootLayer.bounds
+        rootLayer.addSublayer(previewLayer)
+
+        //Device orientation
+        let curDeviceOrientation = UIDevice.current.orientation
+        let exifOrientation: CGImagePropertyOrientation
+
+        switch curDeviceOrientation {
+        	// Device oriented vertically, home button on the top
+        	case UIDeviceOrientation.portraitUpsideDown: exifOrientation = .left
+
+        	// Device oriented horizontally, home button on the right
+        	case UIDeviceOrientation.landscapeLeft: exifOrientation = .upMirrored
+
+        	// Device oriented horizontally, home button on the left
+        	case UIDeviceOrientation.landscapeRight: exifOrientation = .down
+
+        	// Device oriented vertically, home button on the bottom
+        	case UIDeviceOrientation.portrait: exifOrientation = .up
+        	
+        	default: exifOrientation = .up
+        }
+
+        //Core ML classifier (elevator buttons)
+        let visionModel = try VNCoreMLModel(for: MLModel(contentsOf: modelURL)) //loading model
+
+        let objectRecognition = VNCoreMLRequest(model: visionModel, completionHandler: { (request, error) in
+            DispatchQueue.main.async(execute: {
+                // perform all the UI updates on the main queue
+                if let results = request.results {
+                    self.drawVisionRequestResults(results)
+                }
+            })
+        })
+
+        //Parse recognized object observations
+        for observation in results where observation is VNRecognizedObjectObservation {
+            guard let objectObservation = observation as? VNRecognizedObjectObservation else { continue }
+
+            // Select only the label with the highest confidence.
+            let topLabelObservation = objectObservation.labels[0]
+            let objectBounds = VNImageRectForNormalizedRect(objectObservation.boundingBox, Int(bufferSize.width), Int(bufferSize.height))
+            
+            let shapeLayer = self.createRoundedRectLayerWithBounds(objectBounds)
+            
+            let textLayer = self.createTextSubLayerInBounds(objectBounds, identifier: topLabelObservation.identifier, confidence: topLabelObservation.confidence)
+            shapeLayer.addSublayer(textLayer)
+            detectionOverlay.addSublayer(shapeLayer)
+        }
+
+
     }
 
 
