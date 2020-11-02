9e574d7dea8ab5de250df099acd074f811103e11
Gianluca Coletti <55032531+GColetti@users.noreply.github.com>
2020-11-02 18:10:00 -0500

    Changes

--- ButtonLocator/ButtonLocator/ViewController.swift
+++ ButtonLocator/ButtonLocator/ViewController.swift
@@ -107,10 +107,10 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         let exifOrientation: CGImagePropertyOrientation
         
         switch curDeviceOrientation {
-        case UIDeviceOrientation.portraitUpsideDown:  // Device oriented vertically, home button on the top
-            exifOrientation = .left
-        case UIDeviceOrientation.landscapeLeft:       // Device oriented horizontally, home button on the right
-            exifOrientation = .upMirrored
+//        case UIDeviceOrientation.portraitUpsideDown:  // Device oriented vertically, home button on the top
+//            exifOrientation = .left
+//        case UIDeviceOrientation.landscapeLeft:       // Device oriented horizontally, home button on the right
+//            exifOrientation = .upMirrored
         case UIDeviceOrientation.landscapeRight:      // Device oriented horizontally, home button on the left
             exifOrientation = .down
         case UIDeviceOrientation.portrait:            // Device oriented vertically, home button on the bottom
