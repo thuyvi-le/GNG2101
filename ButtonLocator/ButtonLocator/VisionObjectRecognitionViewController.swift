639f9c01646e35c033d461db59047112ee6e968d
Gianluca Coletti <55032531+GColetti@users.noreply.github.com>
2020-11-03 14:31:26 -0500

    Fixes

--- ButtonLocator/ButtonLocator/VisionObjectRecognitionViewController.swift
+++ ButtonLocator/ButtonLocator/VisionObjectRecognitionViewController.swift
@@ -20,7 +20,7 @@ class VisionObjectRecognitionViewController: ViewController {
         // Setup Vision parts
         let error: NSError! = nil
         
-        guard let modelURL = Bundle.main.url(forResource: "ButtonDetection", withExtension: "mlproj") else {
+        guard let modelURL = Bundle.main.url(forResource: "ButtonDetection", withExtension: "mlmodel") else {
             return NSError(domain: "VisionObjectRecognitionViewController", code: -1, userInfo: [NSLocalizedDescriptionKey: "Model file is missing"])
         }
         do {
