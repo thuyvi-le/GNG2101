747695d2fa865ad0258d1baa85e7e0ed03578c8e
Gianluca Coletti <55032531+GColetti@users.noreply.github.com>
2020-11-18 15:16:56 -0500

    Changes

--- ButtonLocator/ButtonLocator/AppDelegate.swift
+++ ButtonLocator/ButtonLocator/AppDelegate.swift
@@ -11,8 +11,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        Thread.sleep(forTimeInterval: 1.0)
         // Override point for customization after application launch.
         return true
     }
